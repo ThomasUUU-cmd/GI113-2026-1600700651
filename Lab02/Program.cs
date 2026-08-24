40be3fb492fededc5269cb1ed81c02b5a0552926
-pharit_samranchai <-rileyroseboston@gmail.com>
2026-08-24 11:21:49 +0700

    Assign Part A Logic

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -6,14 +6,46 @@
  * Course     : GI113 Computer Programming (GI)
  */
 
-
 namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /// --- Part A --- 
+            /// Kirin's Status Report
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int currentHp = 175;
+            int maxHp = 240;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}\n"+
+                $"Rank: {rank}\n"+
+                $"Level: {level}\n"+
+                $"HP: {currentHp} / {maxHp}\n"+
+                $"Attack Power: {attackPower}\n"+
+                $"Crit Multiplier: {critMultiplier}\n"+
+                $"Is Boss: {isBoss}");
+            Console.WriteLine();
+            Console.WriteLine($"HP Percent: {CalculatePercent(currentHp, maxHp)}%");
+            Console.WriteLine();
+
+            int damageAmount = 60;
+            currentHp -= damageAmount;
+
+            Console.WriteLine($"{bossName} takes {damageAmount} damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}\n" + $"HP Percent: {CalculatePercent(currentHp, maxHp)}%");
+
+            /// --- Part B --- 
+            /// เกมของผมเอง (WIP)
         }
+        static int CalculatePercent(int value, int max) => value * 100 / max; // เคยใช้ (int)((float)value / (float)max * 100) แต่ผมเองก็พึ่งรู้เหมือนกันว่าแค่เปลี่ยน order การคำนวนเป็นแบบนี้มันก็ใช้ได้เหมือนกัน
     }
 }
