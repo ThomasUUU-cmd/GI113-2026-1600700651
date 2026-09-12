395e12a4ecfc2d5b50066527a392813e814ffff2
ThomasUUU-cmd <rileyroseboston@gmail.com>
2026-09-12 08:38:14 +0700

    Add Student Id

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Assignment01
+﻿/*
+* Student ID : 1690700651
+* Name       : Pharit Samranchai
+* Section    : 129A
+* No.        : N/A
+* Course     : GI113 Computer Programming (GI)
+*/
+
+namespace Assignment01
 {
     internal class Program
     {
