73ee7a9679def2c36148d5dc7148b0e8e6eba07a
ThomasUUU-cmd <rileyroseboston@gmail.com>
2026-09-14 10:39:20 +0700

    Assign build-in method tester

--- Lab05/Program.cs
+++ Lab05/Program.cs
@@ -12,7 +12,37 @@ namespace Lab05
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("________________________________");
+            Console.WriteLine("          Slot Container        ");
+            Console.WriteLine("________________________________");
+
+            Console.Write("Create Slot At Index: ");
+            bool slotIndexValid = int.TryParse(Console.ReadLine(), out var slotIndex);
+            if (!slotIndexValid) { Console.WriteLine("INPUT MISMATCH"); return; }
+            Console.Write("Set Slot Value: ");
+            bool slotValueValid = float.TryParse(Console.ReadLine(), out var slotValue);
+            Console.Write("Set Slot Max Stack Size: ");
+            bool slotStackSizeValid = int.TryParse(Console.ReadLine(), out var slotStackSize);
+
+            bool isVariablesValid = slotStackSizeValid && slotValueValid;
+            if (!isVariablesValid) { Console.WriteLine("INPUT MISMATCH"); return; }
+
+            Console.WriteLine();
+            Console.Write("Add Slot Value: ");
+            bool newSlotValueValid = float.TryParse(Console.ReadLine(), out var newSlotValue);
+            if (!newSlotValueValid) { Console.WriteLine("INPUT MISMATCH"); return; }
+            slotValue += newSlotValue;
+            float remainingValue = newSlotValue - (slotStackSize % newSlotValue);
+            slotValue = Math.Clamp(slotValue, 0, slotStackSize);
+
+            Console.WriteLine();
+            Console.WriteLine($"Current slot {slotIndex} value is {slotValue}");
+            Console.WriteLine($"remaining {remainingValue} value");
+
+            Random rnd = new Random();
+            int indexRoll = rnd.Next(0, 10);
+            slotIndex = indexRoll;
+            Console.WriteLine($"Move slot index to {indexRoll}");
         }
     }
 }
